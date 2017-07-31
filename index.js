773637270e7792898cd8b64e57427fb48219229f
huy tran <buihuytran.com@gmail.com>
2017-07-31 22:48:53 +0700

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.set("views","./views");
 var server =require("http").Server(app);
 var io =require("socket.io")(server);
-server.listen(process.env.port ||3000);
+server.listen(process.env.PORT ||3000);
 var listuser=[];
 io.on("connection",function(socket){
 	console.log('co nguoi ket noi '+socket.id);
